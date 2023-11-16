342e250cae497db72741bc993ebac8145e9434e4
Tr14 <xuantrucx111@gmail.com>
2023-11-16 15:04:33 +0700

    pulsar1

--- pulsar.js
+++ pulsar.js
@@ -3,31 +3,27 @@ const Pulsar = require('pulsar-client');
 (async () => {
     // Create a client
     const client = new Pulsar.Client({
-        serviceUrl: 'pulsar://localhost:6650'
+        serviceUrl: 'pulsar://localhost:6650',
+        operationTimeoutSeconds: 30,
     });
 
     // Create a producer
     const producer = await client.createProducer({
         topic: 'persistent://public/default/my-topic',
+        sendTimeoutMs: 30000,
+        batchingEnabled: true,
     });
 
-    // Create a consumer
-    const consumer = await client.subscribe({
-        topic: 'persistent://public/default/my-topic',
-        subscription: 'sub1'
-    });
-
-    // Send a message
-    producer.send({
-        data: Buffer.from("hello")
-    });
-
-    // Receive the message
-    const msg = await consumer.receive();
-    console.log(msg.getData().toString());
-    consumer.acknowledge(msg);
+    // Send messages
+    for (let i = 0; i < 10; i += 1) {
+        const msg = `my-message-${i}`;
+        producer.send({
+            data: Buffer.from(msg),
+        });
+        console.log(`Sent message: ${msg}`);
+    }
+    await producer.flush();
 
     await producer.close();
-    await consumer.close();
     await client.close();
 })();
