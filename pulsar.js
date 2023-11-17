272487e4c13e08346e76520c9123eaaed2c2df82
Tr14 <xuantrucx111@gmail.com>
2023-11-17 14:12:31 +0700

    pulsar1

--- pulsar.js
+++ pulsar.js
@@ -1,26 +1,32 @@
-const Pulsar = require('pulsar-client');
+const pulsar = require('pulsar-client');
 
-(async () => {
-    const client = new Pulsar.Client({
-        serviceUrl: 'pulsar+ssl://localhost:6651',
-        authentication: {
-            use: 'tls',
-            trustedCertificates: ['/home/trucnguyen/apache-pulsar-3.1.1/cert/ca-cert.pem'],
-            certificatePath: '/home/trucnguyen/apache-pulsar-3.1.1/cert/pulsar-admin-cert.pem',
-            privateKeyPath: '/home/trucnguyen/apache-pulsar-3.1.1/cert/pulsar-admin.pem',
-        },
-    });
+async function main() {
+    try {
+        const client = new pulsar.Client({
+            serviceUrl: 'pulsar+ssl://your-pulsar-broker-hostname:6651',
+            authentication: {
+                use: 'tls',
+                trustedCertificates: ['/home/trucnguyen/apache-pulsar-3.1.1/cert/ca-cert.pem'],
+                certificatePath: '/home/trucnguyen/apache-pulsar-3.1.1/cert/pulsar-admin-cert.pem',
+                privateKeyPath: '/home/trucnguyen/apache-pulsar-3.1.1/cert/pulsar-admin.pem',
+            },
+        });
 
-    const producer = await client.createProducer({
-        topic: 'my-topic',
-    });
+        const producer = await client.createProducer({
+            topic: 'your-topic',
+        });
 
-    const message = {
-        data: 'Hello, Pulsar!',
-    };
+        const message = {
+            data: 'Hello, Pulsar!',
+        };
 
-    await producer.send(message);
+        await producer.send(message);
 
-    await producer.close();
-    await client.close();
-})();
+        await producer.close();
+        await client.close();
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+main();
