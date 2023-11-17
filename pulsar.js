7ae3a77c54403adb75f9886706aff6085a2a8f73
Tr14 <xuantrucx111@gmail.com>
2023-11-17 14:13:44 +0700

    pulsar1

--- pulsar.js
+++ pulsar.js
@@ -3,17 +3,17 @@ const pulsar = require('pulsar-client');
 async function main() {
     try {
         const client = new pulsar.Client({
-            serviceUrl: 'pulsar+ssl://your-pulsar-broker-hostname:6651',
+            serviceUrl: 'pulsar+ssl://localhost:6651',
             authentication: {
                 use: 'tls',
                 trustedCertificates: ['/home/trucnguyen/apache-pulsar-3.1.1/cert/ca-cert.pem'],
-                certificatePath: '/home/trucnguyen/apache-pulsar-3.1.1/cert/pulsar-admin-cert.pem',
-                privateKeyPath: '/home/trucnguyen/apache-pulsar-3.1.1/cert/pulsar-admin.pem',
+                certificatePath: '/home/trucnguyen/apache-pulsar-3.1.1/cert/pulsar-server-cert.pem',
+                privateKeyPath: '/home/trucnguyen/apache-pulsar-3.1.1/cert/pulsar-server.pem',
             },
         });
 
         const producer = await client.createProducer({
-            topic: 'your-topic',
+            topic: 'my-topic',
         });
 
         const message = {
