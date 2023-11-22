d2bb67631f34a1fbbe9b4dafeca6d383d80981ed
Tr14 <xuantrucx111@gmail.com>
2023-11-22 11:34:46 +0700

    pulsar1

--- getData.js
+++ getData.js
@@ -2,7 +2,7 @@ const { pool } = require("./db");
 
 async function getData() {
     try {
-        const res = await pool.query("SELECT * FROM pulsardata");
+        const res = await pool.query("SELECT * FROM pulsardata WHERE message = 'truc test json'");
         console.log(res.rows);
     } catch (error) {
         console.error(error);
